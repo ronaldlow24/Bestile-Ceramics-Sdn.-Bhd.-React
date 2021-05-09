4cb0d0446cfea6eb075775936acbb7e8eb63d566
ronald <ron556611@gmail.com>
2021-05-09 21:51:01 +0800

    test

--- src/Component/Project.js
+++ src/Component/Project.js
@@ -6,17 +6,17 @@ import React, { useEffect } from "react";
 import "./AOS";
 
 function Project() {
-  let { albumID } = useParams();
-  let history = useHistory();
+  // let { albumID } = useParams();
+  // let history = useHistory();
 
-  useEffect(() => {
-    if (albumID) {
-      window.open(
-        `https://www.facebook.com/media/set/?vanity=759558020844572&set=a.${albumID}`
-      );
-      history.push("/work");
-    }
-  });
+  // useEffect(() => {
+  //   if (albumID) {
+  //     window.open(
+  //       `https://www.facebook.com/media/set/?vanity=759558020844572&set=a.${albumID}`
+  //     );
+  //     history.push("/work");
+  //   }
+  // });
 
   return (
     <>

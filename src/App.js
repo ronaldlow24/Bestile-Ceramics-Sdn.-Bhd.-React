7c3bc7b0c8731809f813a5878381beac06a824df
Ronald Low Cheng Young <42057616+ronaldlow24@users.noreply.github.com>
2021-05-28 18:47:47 +0800

    fix schroll behavior when navigate

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import Footer from "./Footer";
 import Router from "./Router";
 import { BrowserRouter } from "react-router-dom";
 import ScrollToTop from "react-scroll-to-top";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+function ScrollToTopWhenNavigate() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
@@ -11,6 +23,7 @@ function App() {
       <Header />
       <Router />
       <Footer />
+      <ScrollToTopWhenNavigate />
       <ScrollToTop smooth style={{ zIndex: "9999" }} />
     </BrowserRouter>
   );

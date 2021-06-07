2251e309fdd2029fe5ba1de11f2f5726ab5b5ca6
Ronald Low Cheng Young <42057616+ronaldlow24@users.noreply.github.com>
2021-06-07 17:02:40 +0800

    update image url

--- src/Data/projectData.js
+++ src/Data/projectData.js
@@ -1,63 +1,48 @@
 const projectData = [
   {
-    src:
-      "https://scontent.fmkz1-1.fna.fbcdn.net/v/t1.18169-9/r90/10400237_786051541528553_3801352568114881212_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=cdbe9c&_nc_ohc=wj4_xbemlAYAX89GgWU&_nc_ht=scontent.fmkz1-1.fna&_nc_tp=31&oh=a8f49e30fc75eff4ff143b0168f8c63d&oe=60BE47FD",
+    src: "/img/10400237_786051541528553_3801352568114881212_n.jpg",
     albumTitle: "Furnising Design",
     albumID: "786051444861896",
   },
   {
-    src:
-      "https://scontent.fmkz1-1.fna.fbcdn.net/v/t31.18172-8/15443027_958916514242054_847750585084372253_o.jpg?_nc_cat=106&ccb=1-3&_nc_sid=cdbe9c&_nc_ohc=q1D9PwETAUYAX_-kh4N&_nc_ht=scontent.fmkz1-1.fna&oh=da96c5a0631ab1e26019c525b7b6f015&oe=60BAD71C",
+    src: "/img/15540791_958916554242050_7749701708940716350_o.jpg",
     albumTitle: "Pebble wash",
     albumID: "958915947575444",
   },
   {
-    src:
-      "https://scontent.fmkz1-1.fna.fbcdn.net/v/t31.18172-8/12794909_787377268062647_115369294413569696_o.jpg?_nc_cat=100&ccb=1-3&_nc_sid=cdbe9c&_nc_ohc=NugYMHW8YJAAX_lPHO9&_nc_ht=scontent.fmkz1-1.fna&oh=71513ffd78e7fb5d82de48f44062be64&oe=60BAF59E",
+    src: "/img/13139383_822978817835825_6152493147881684461_n.jpg",
     albumTitle: "Natural Stone",
     albumID: "787373388063035",
   },
   {
-    src:
-      "https://scontent.fmkz1-1.fna.fbcdn.net/v/t1.18169-9/12998678_810206399113067_2527918920304434312_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=cdbe9c&_nc_ohc=GTLpSh5MTYAAX8zi6Ha&_nc_ht=scontent.fmkz1-1.fna&oh=d0b3f26efe61ea16f236d7d2d74ad42c&oe=60BD189B",
+    src: "/img/12998678_810206399113067_2527918920304434312_n.jpg",
     albumTitle: "Government Project - Jakoa Kluang",
     albumID: "810204809113226",
   },
   {
-    src:
-      "https://scontent.fmkz1-1.fna.fbcdn.net/v/t1.18169-9/1915596_798867980246909_469725381571269575_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=cdbe9c&_nc_ohc=F0fJ7Wn2cdIAX_nCDwA&_nc_ht=scontent.fmkz1-1.fna&oh=585ac5048af2e4b8cfba85eab5272e78&oe=60BD7245",
+    src: "/img/13000350_810204425779931_8080760380363624528_n.jpg",
     albumTitle: "Bath Talk",
     albumID: "805945342872506",
   },
   {
-    src:
-      "https://scontent.fmkz1-1.fna.fbcdn.net/v/t31.18172-8/981216_787447571388950_4695325606871267585_o.jpg?_nc_cat=101&ccb=1-3&_nc_sid=cdbe9c&_nc_ohc=-8GLHv1gSGUAX-88CT4&_nc_ht=scontent.fmkz1-1.fna&oh=bcd3db7d771e0b261aa8f62046bbbe8f&oe=60BB79B5",
+    src: "/img/981216_787447571388950_4695325606871267585_o.jpg",
     albumTitle: "Kitchen Cabinet Design",
     albumID: "787446658055708",
   },
   {
-    src:
-      "https://scontent.fmkz1-1.fna.fbcdn.net/v/t1.18169-9/12923320_807738539359853_8804635717204989935_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=cdbe9c&_nc_ohc=-oiSk-PEBYMAX8O31fl&_nc_ht=scontent.fmkz1-1.fna&oh=bf9b73f1475b15b64ca90dc5063bedec&oe=60BDF8AE",
+    src: "/img/12923320_807738539359853_8804635717204989935_n.jpg",
     albumTitle: "25th Storey Condominium Project",
     albumID: "807734999360207",
   },
   {
-    src:
-      "https://scontent.fmkz1-1.fna.fbcdn.net/v/t1.18169-9/12923337_805941192872921_750030827027561360_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=cdbe9c&_nc_ohc=Dsz_dZ7vTegAX9IsLlX&_nc_ht=scontent.fmkz1-1.fna&oh=c6a80133ac0360c9c4b708c59e9bc9a6&oe=60BB618E",
+    src: "/img/12717998_805941229539584_3002098384592778811_n.jpg",
     albumTitle: "Swimming Pool",
     albumID: "805940029539704",
   },
   {
-    src:
-      "https://scontent.fmkz1-1.fna.fbcdn.net/v/t31.18172-8/12829529_786147771518930_9087593093927216370_o.jpg?_nc_cat=105&ccb=1-3&_nc_sid=cdbe9c&_nc_ohc=xzeRQDo6oBcAX_ClPEF&tn=AsNZYEJjiGUId8YA&_nc_ht=scontent.fmkz1-1.fna&oh=f60ab034f65e862035663d0bb1669099&oe=60BD606C",
+    src: "/img/11252866_786147654852275_1067107121856954679_o.jpg",
     albumTitle: "Istana Besar Johor",
     albumID: "786141974852843",
-  },
-  {
-    src:
-      "https://scontent.fmkz1-1.fna.fbcdn.net/v/t31.18172-8/12657366_787370678063306_6379268201122624840_o.jpg?_nc_cat=100&ccb=1-3&_nc_sid=cdbe9c&_nc_ohc=jgwzh4JOtUsAX9gsXJw&_nc_ht=scontent.fmkz1-1.fna&oh=8f2ca5929e457c3c2518c00810e9e6b9&oe=60BB3B8E",
-    albumTitle: "Natural Stone 2",
-    albumID: "787370128063361",
   },
 ];
 
